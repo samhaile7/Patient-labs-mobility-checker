e52e91c4c36bc8f97f3fbecd50f2bd43f3ecca93
Samuel Haile <samhaile129@gmail.com>
2022-12-18 11:50:57 -0500

    Simplified add_lab_values method

--- patient.py
+++ patient.py
@@ -9,12 +9,14 @@ class Patient:
                              'insurance_company': insurer}
         self.labs = {}
 
+    #Receives new lab value input and appends to patient labs attribute
     def add_lab_values(self):
         new_lab_entry = None
         while new_lab_entry != "done":
-            new_lab_entry = input("Which lab has new values? (Use format PTT = 1.3 when entering values\n "
+            new_lab_entry = input("Enter new labs(Use format PTT = 1.3 when entering values\n "
                                   "Write 'done' when finished.")
             new_entry_list = new_lab_entry.split("=")
             new_lab_name = new_entry_list[0].strip()
-            new_lab_value = float(new_entry_list[1].strip())
+            new_lab_value_string = float(new_entry_list[1].strip())
+            new_lab_value = int(new_lab_value_string)
             self.labs[new_lab_name] = new_lab_value
